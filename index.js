8d51b219a283b557328112a472d069668b0ceb1f
Joe Dahlquist <joedahlquist@gmail.com>
2012-11-04 23:50:40 -0800

    Fixed to check $.prototype.jquery, not $.jquery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var $ = global.jQuery || global.$;
-if (typeof $ === 'undefined' || typeof $.jquery === 'undefined')
+if (typeof $ === 'undefined' || typeof $.prototype.jquery === 'undefined')
   throw new Error('Failed to load module jquery-cjs: jQuery is not defined in the global scope.');
 module.exports = $;
